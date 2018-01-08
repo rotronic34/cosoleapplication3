5f27e31fafbceb8247c4847c1df4ca2b3163d3cf
Roeut Chak <roeut.chak@gmail.com>
2018-01-08 01:42:38 -0500

    created class, created data field, and one with manual setters and getters

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -2,8 +2,19 @@
 
 namespace ConsoleApplication3
 {
-    class Program
+    class Animal
     {
+        public double height { get; set; }
+        public double weight { get; set; }
+        public string sound { get; set; }
+
+        public string name;
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
