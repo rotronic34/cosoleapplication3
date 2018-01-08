6e208b761b2dd49032fafd64ef2296c8fd20bbd7
Roeut Chak <roeut.chak@gmail.com>
2018-01-08 02:03:10 -0500

    created getters and setters for the Animal Class

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -14,6 +14,28 @@ namespace ConsoleApplication3
             set { name = value; }
         }
 
+        public Animal(){            //constructor for Animal Class
+            this.height = 0;
+            this.weight = 0;
+            this.name = "No name";
+            this.sound = "No sound";
+            numOfAnimals++;
+        }
+
+        public Animal(double height, double weight, string sound, string name){
+            this.height = height;
+            this.weight = weight;
+            this.name = name;
+            this.sound = sound;
+            numOfAnimals++;
+        }
+
+        static int numOfAnimals = 0;  //static field attribute; check number of animal objects created
+
+        public static int getNumOfAnimals(){
+            return numOfAnimals;      //return numOfAnimals whenever it is called
+        }
+
 
         static void Main(string[] args)
         {
