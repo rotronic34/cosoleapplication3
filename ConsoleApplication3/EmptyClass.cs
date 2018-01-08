9fedc6557bb012b614dc8188a4c9b650d5123024
Roeut Chak <roeut.chak@gmail.com>
2018-01-08 01:22:42 -0500

    starting commits at class tutorial; tutorial with derek banas

---
+++ ConsoleApplication3/EmptyClass.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ConsoleApplication3 {
+    class Program {
+        static void main(string[] args) {
+            Console.WriteLine("Hello Roeut");
+        }
+    }
+}
---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
